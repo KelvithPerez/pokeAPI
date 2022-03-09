8739f83ad3bdac2feac00d2110215738d9605070
Kelvith Perez <kelvith@gmail.com>
2022-03-09 16:01:22 -0600

    pokeAPI

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,22 @@
-const Character = () => {
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+const Character = async () => {
+  const id = getHash();
+  const character = await getData(id);
     const view = `
      <div class="Characters-inner">
        <article class="Characters-card">
-         <img src="image" alt="name">
-         <h2>Name</h2>
+         <img src="${character.imege}" alt="${character.name}">
+         <h2>${character.name}</h2>
        </article>
        <article class="Characters-card">
-         <h3>Episodios:</h3>
-         <h3>Tipo:</h3>
-         <h3>Especies:</h3>
-         <h3>Genero:</h3>
-         <h3>Origen:</h3>
-         <h3>Localisación:</h3>
+         <h3>Episodios: <span>${character.episodio.length}</span></h3>
+         <h3>Tipo: <span>${character.tipo}</span></h3>
+         <h3>Especies: <span>${character.especies}</span></h3>
+         <h3>Genero: <span>${character.genero}</span></h3>
+         <h3>Origen: <span>${character.origen.name}</span></h3>
+         <h3>Localisación: <span>${character.localizacion.name}</span></h3>
        </article>
      </div>
     `;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const character = await getData();
     const view = `
     <div class="Characters">
+     ${characters.results.map(character => `
      <article class="Character-item">
-       <a href="#/1/">
-         <img src="image" ait="name">
-         <h2>Name</h2>
-       </a>
-     </article>
+        <a href="#/${character.id}/">
+          <img src="${character.image}" ait="${character.name}">
+          <h2>${character.name}</h2>
+        </a>
+      </article>
+      `).join('')}
     </div>
     `;
     return view;
